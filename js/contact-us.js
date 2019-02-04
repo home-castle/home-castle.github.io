2bc18fb7a71e556ef1478cfb57053025bfdb30c0
Jorge Aparicio <jorge.aparicio@pomona.edu>
2019-02-03 22:31:32 -0800

    working contact form not set to proper email until moved over to proper aws account

--- js/contact-us.js
+++ js/contact-us.js
@@ -1,64 +1,64 @@
 
-function submitToAPI(e) {
-       e.preventDefault();
-       var URL =  "https://hc8nkjsg0k.execute-api.us-west-2.amazonaws.com/Test01";
-            var Namere = /[A-Za-z]{1}[A-Za-z]/;
-            if (!Namere.test($("#name-input").val())) {
-                         alert ("Name can not less than 2 char");
-                return;
-            }
-            var mobilere = /[0-9]{10}/;
-            if (!mobilere.test($("#phone-input").val())) {
-                alert ("Please enter valid mobile number");
-                return;
-            }
-            if ($("#email-input").val()=="") {
-                alert ("Please enter your email id");
-                return;
-            }
+let btn = document.querySelector("#submit-btn");
 
-            var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
-            if (!reeamil.test($("#email-input").val())) {
-                alert ("Please enter valid email address");
-                return;
-            }
+btn.addEventListener("click", submit);
 
-       var name = $("#name-input").val();
-       var phone = $("#phone-input").val();
-       var email = $("#email-input").val();
-       var desc = $("#description-input").val();
-       var data = {
-          name : name,
-          phone : phone,
-          email : email,
-          desc : desc
-        };
+function submit(e) {
+  e.preventDefault();
+      var Namere = /[A-Za-z]{1}[A-Za-z]/;
+      if (!Namere.test($("#form-name").val())) {
+          alert ("Name can not less than 2 char");
+          return;
+       }
+      var mobilere = /[0-9]{10}/;
+      if (!mobilere.test($("#form-phone").val())) {
+          alert ("Please enter valid mobile number");
+          return;
+       }
+      var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+      if (!reeamil.test($("#form-email").val())) {
+          alert ("Please enter valid email address");
+          return;
+       }
 
-       $.ajax({
-         type: "POST",
-         url : "https://hc8nkjsg0k.execute-api.us-west-2.amazonaws.com/Test01",
-         dataType: "json",
-         crossDomain: "true",
-         contentType: "application/json; charset=utf-8",
-         data: JSON.stringify(data),
+  let name = document.querySelector("#form-name");
+  let email = document.querySelector("#form-email");
+  let message = document.querySelector("#description");
+  let phone = document.querySelector("#form-phone");
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://mc5uvkcf0j.execute-api.us-west-2.amazonaws.com/dev", true);
+
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+    }
+  };
+  var data = {
+    name: name.value,
+    phone: phone.value,
+    email: email.value,
+    message: message.value
+  };
+
+  if (name.value && email.value && message.value) {
+     document.querySelector('.alert-success').style.display = 'block';
+     xhr.send(JSON.stringify(data));
+     //Hide alert after 5 seconds
+     setTimeout(function(){
+      document.querySelector('.alert-success').style.display = 'none'; },5000);
 
 
-         success: function () {
-           //show alert
-           document.querySelector('.alert-success').style.display = 'block';
+      document.getElementById('contact-form').reset();
 
-           //Hide alert after 5 seconds
-           setTimeout(function(){
-           document.querySelector('.alert-success').style.display = 'none'; },5000);
-           document.getElementById('contactForm').reset();
-       location.reload();
-         },
-         error: function () {
-           // show an error message
-            document.querySelector('.alert-fail').style.display = 'block';
+  } else {
+         // show an error message
+          document.querySelector('.alert-fail').style.display = 'block';
 
-            //Hide alert after 5 seconds
-            setTimeout(function(){
-            document.querySelector('.alert-fail').style.display = 'none'; },5000);
-         }});
-     }
+          //Hide alert after 5 seconds
+          setTimeout(function(){
+          document.querySelector('.alert-fail').style.display = 'none'; },5000);
+  }
+}
+
+
+
