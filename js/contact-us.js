f3ffc152b89303084da8d553d1325a481a4526ab
Jorge Aparicio <43185027+jorge-aparicio@users.noreply.github.com>
2019-08-08 13:46:08 -0700

    Update contact-us.js

--- js/contact-us.js
+++ js/contact-us.js
@@ -26,7 +26,7 @@ function submit(e) {
   let message = document.querySelector("#description");
   let phone = document.querySelector("#form-phone");
   var xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://jo7bef0pt3.execute-api.us-west-2.amazonaws.com/contact-1", true);
+  xhr.open("POST", "https://v69j50892h.execute-api.us-west-2.amazonaws.com/dev", true);
 
   xhr.setRequestHeader("Content-type", "application/json");
   xhr.onreadystatechange = function() {
