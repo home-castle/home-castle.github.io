86e382738644a8f178de7d8be34ca8fc7377bf93
Jason Cabrera <cabrerajason7@gmail.com>
2022-02-15 23:32:05 -0500

    removed carousel

--- js/gallery-loader.js
+++ js/gallery-loader.js
@@ -1,21 +1,43 @@
 // get container for images
 let imagesDiv = $(".images-container");
 
+// get bootstrap carousel
+let carousel = $(".carousel-inner");
+
 // iterate over images folder
 let totalImages = 3;
 for (let imageNum = 1; imageNum <= totalImages; imageNum++) {
+    let imgSrc = "images/image" + imageNum + ".jpg";
+
     // create new image
-    let img = new Image();
+    let img1 = new Image();
+    let img2 = new Image();
 
     // setting img properties
-    img.src = "images/image" + imageNum + ".jpg";
-    img.className = "gallery-img";
+    img1.src = imgSrc;
+    img1.className = "gallery-img";
+    img1.id = "" + (imageNum - 1);
+    img2.src = imgSrc;
+    img2.className = "gallery-img";
 
-    // append img to container
-    imagesDiv.append(img);
+    // append img to all images container
+    imagesDiv.append(img1);
+
+    // append img to bootstrap carousel
+    let div = document.createElement('div');
+    div.className = "carousel-item";
+    div.id = img2.src;
+
+    if (imageNum === 1) {
+        div.className = div.className + " active";
+    }
+
+    img2.className = "gallery-img " + "d-block w-100";
+    div.append(img2);
+    carousel.append(div);
 }
 
-
+// image on click function
 $("img").click(function () {
-    window.alert("Img clicked");
+    window.alert("image clicked");
 });
