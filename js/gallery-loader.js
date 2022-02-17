85d9c9afa0950b962871cde342b2306127da2936
Jason Cabrera <cabrerajason7@gmail.com>
2022-02-17 00:17:43 -0500

    added photos to gallery

--- js/gallery-loader.js
+++ js/gallery-loader.js
@@ -5,39 +5,21 @@ let imagesDiv = $(".images-container");
 let carousel = $(".carousel-inner");
 
 // iterate over images folder
-let totalImages = 3;
+let totalImages = 11;
 for (let imageNum = 1; imageNum <= totalImages; imageNum++) {
-    let imgSrc = "images/image" + imageNum + ".jpg";
+    let imgSrc = "gallery/image" + imageNum + ".jpg";
 
     // create new image
     let img1 = new Image();
     let img2 = new Image();
 
     // setting img properties
-    img1.src = imgSrc;
-    img1.className = "gallery-img";
-    img1.id = "" + (imageNum - 1);
-    img2.src = imgSrc;
-    img2.className = "gallery-img";
+    let img = `<a href=${imgSrc} data-lightbox="image-1" data-title="My Caption">
+                    <img class="gallery-img" src=${imgSrc} />
+                </a>`;
+
+
 
     // append img to all images container
-    imagesDiv.append(img1);
-
-    // append img to bootstrap carousel
-    let div = document.createElement('div');
-    div.className = "carousel-item";
-    div.id = img2.src;
-
-    if (imageNum === 1) {
-        div.className = div.className + " active";
-    }
-
-    img2.className = "gallery-img " + "d-block w-100";
-    div.append(img2);
-    carousel.append(div);
+    imagesDiv.append(img);
 }
-
-// image on click function
-$("img").click(function () {
-    window.alert("image clicked");
-});
