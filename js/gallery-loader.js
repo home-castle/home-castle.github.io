3e0db6f65204c1e38199b3112685b4120abf558c
strawhatjay <cabrerajason7@gmail.com>
2022-02-21 19:14:28 -0500

    more changes for gallery

--- js/gallery-loader.js
+++ js/gallery-loader.js
@@ -4,20 +4,50 @@ let imagesDiv = $(".images-container");
 // get bootstrap carousel
 let carousel = $(".carousel-inner");
 
+
+/*
+if (screenWidth >= 1100) {
+    screenWidth = ;
+} else if (screenWidth <= )
+
+ */
 // iterate over images folder
-let totalImages = 11;
+let totalImages = 9;
+let currentDiv = $('<div></div>').addClass("gallery-row");
+let totalWidth = imagesDiv.innerWidth();
+
+let imgWidth;
+let imgHeight;
+let screenWidth = screen.width;
+
 for (let imageNum = 1; imageNum <= totalImages; imageNum++) {
+    // define image src
     let imgSrc = "gallery/image" + imageNum + ".jpg";
 
     // create new image
-    let img1 = new Image();
-    let img2 = new Image();
+    let img = $('<a></a>')
+                .attr("data-lightbox", "image-1")
+                .attr("data-title", "My Caption")
+                .attr("href", imgSrc)
+                .append(
+                  $('<img></img>')
+                      .addClass("gallery-img")
+                      .attr("src", imgSrc)
+                );
 
-    // setting img properties
-    let img = `<a href=${imgSrc} data-lightbox="image-1" data-title="My Caption">
-                    <img class="gallery-img" src=${imgSrc} />
-                </a>`;
 
     // append img to all images container
     imagesDiv.append(img);
 }
+
+/*
+if (imagesCurrRow != 0) {
+    if (imagesCurrRow != imagesPerRow) {
+        currentDiv.removeClass("gallery-row");
+        currentDiv.addClass("gallery-row-last");
+    }
+
+    imagesDiv.append(currentDiv);
+}
+
+ */
