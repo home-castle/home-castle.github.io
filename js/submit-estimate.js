fcc7fcd30f7423e501b7b2c639cf0ac65a09842e
strawhatjay <cabrerajason7@gmail.com>
2022-04-29 00:53:29 -0400

    gui for free estimate page pretty much complete

--- js/submit-estimate.js
+++ js/submit-estimate.js
@@ -2,9 +2,54 @@ $("form").on( "submit", function (e) {
     // prevent page from refreshing
     e.preventDefault();
 
-    // hide form and display loading scroll
-    $(this).hide();
-    $('#sending-form').show();
+    let formFeedback = $("#sending-form");
 
-    // validate required inputs
+    // hide form and display loading scroll
+    formFeedback.show();
+
+    // retrieve form values
+    let name = $("#name");
+    let phone = $("#phone-number");
+    let email = $("#email");
+    let emailPreferred = $("#email-contact").is(":checked");
+    let phonePreferred = $("#phone-contact").is(":checked");
+    let address1 = $("#street-address");
+    let address2 = $("#address-line-2");
+    let jobTypeIsExteriorPainting = $("#exterior-painting").is(":checked");
+    let jobTypeIsInteriorPainting = $("#interior-painting").is(":checked");
+    let jobTypeIsFenceGate = $("#fence-gate").is(":checked");
+    let jobTypeIsOther = $("#other").is(":checked");
+    let city = $("#city");
+    let zip = $("#zip-code");
+    let projectDetails = $("#project-details");
+
+    // check if name left blank
+    if (name.val() === "") {
+        name.addClass("is-invalid");
+        $("#name-text").hide();
+    }
+
+    // check if phone left blank
+    if (phone.val() === "") {
+        phone.addClass("is-invalid");
+        $("#phone-number-text").hide();
+    } else if (phone.val().match("^(1-)?\d{3}-\d{3}-\d{4}$")) {
+        window.alert("pattern satisfied");
+    }
+
+    // check if user wants to be contacted through email but didn't insert email
+    if (emailPreferred && email.val() === "") {
+        email.addClass("is-invalid");
+        $("#email-regex-error").hide();
+    }
+
+    formFeedback.hide();
+    window.alert("form submitted");
 });
+
+$("input").change(function (e) {
+    $(this).removeClass("is-invalid");
+    $("#"+ $(this).attr("id") + "-text").show();
+});
+
+
