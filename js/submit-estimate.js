7491240a097c3909f51b4d64eba8ade54e5d5e0e
strawhatjay <cabrerajason7@gmail.com>
2022-07-07 16:08:44 -0700

    working free estimate page

--- js/submit-estimate.js
+++ js/submit-estimate.js
@@ -1,18 +1,25 @@
+let totalErrors = 0;
 $("form").on( "submit", function (e) {
     // prevent page from refreshing
     e.preventDefault();
 
-    let formFeedback = $("#sending-form");
-    let containsError = false;
+    // disable submit button
+    $("#submit-button").attr("disabled", "disabled");
+    $("#submit-button-default-style").hide();
+    $("#submitting-button-style").show();
 
-    // hide form and display loading scroll
-    formFeedback.show();
+    // hide all submit results before attempting to submit
+    $("#submit-error-text").hide();
+    $("#submit-validation-error-text").hide();
+    $("#submit-success-text").hide();
+
+    let containsError = false;
 
     // basic contact info
     let name = $("#name");
     let phone = $("#phone-number");
     let email = $("#email");
-    let emailPreferred = $("#email-contact").is(":checked");
+    let emailPreferred = $("#email-radio-button").is(":checked");
 
     // address fields
     let address1 = $("#street-address");
@@ -20,12 +27,12 @@ $("form").on( "submit", function (e) {
     let city = $("#city");
     let zip = $("#zip-code");
 
-
     // check if name left blank
     if (name.val() === "") {
         name.addClass("is-invalid");
         $("#name-text").hide();
         containsError = true;
+        totalErrors++;
     }
 
     // check if phone left blank
@@ -33,8 +40,7 @@ $("form").on( "submit", function (e) {
         phone.addClass("is-invalid");
         $("#phone-number-text").hide();
         containsError = true;
-    } else if (phone.val().match("^(1-)?\d{3}-\d{3}-\d{4}$")) {
-        window.alert("pattern satisfied");
+        totalErrors++;
     }
 
     // check if user wants to be contacted through email but didn't insert email
@@ -42,6 +48,7 @@ $("form").on( "submit", function (e) {
         email.addClass("is-invalid");
         $("#email-regex-error").hide();
         containsError = true;
+        totalErrors++;
     }
 
     // check if street address field is empty
@@ -49,6 +56,7 @@ $("form").on( "submit", function (e) {
         address1.addClass("is-invalid");
         $("#street-address-text").hide();
         containsError = true;
+        totalErrors++;
     }
 
     // check if city field is empty
@@ -56,6 +64,7 @@ $("form").on( "submit", function (e) {
         city.addClass("is-invalid");
         $("#city-text").hide();
         containsError = true;
+        totalErrors++;
     }
 
     // check if zip code is empty
@@ -63,11 +72,18 @@ $("form").on( "submit", function (e) {
         zip.addClass("is-invalid");
         $("#zip-code-text").hide();
         containsError = true;
+        totalErrors++;
     }
 
     // don't send request if it contains error
     if (containsError) {
-        formFeedback.hide();
+        $("#submit-validation-error-text").show();
+        $("#submit-button").removeAttr("disabled")
+            .removeClass("btn-success")
+            .addClass("button-default")
+            .text("Submit");
+        $("#submit-button-default-style").show();
+        $("#submitting-button-style").hide();
         return;
     }
 
@@ -99,22 +115,84 @@ $("form").on( "submit", function (e) {
         projectDetails: $("#project-details").val()
     };
 
-    console.log("about to call aws function...");
     // send data to aws lambda function
     fetch("https://sl7yfoc57irh5vd6rawzbxuni40luxjb.lambda-url.us-east-2.on.aws/", {
         method: "POST",
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(dataToSend)
     }).then(res => {
-        console.log("Request complete! response:", res);
+        // successfully created estimate
+        if (res.status === 200) {
+            $("#submit-success-text").show();
+            $("#submit-button").removeClass("button-default")
+                .addClass("btn-success")
+                .text("Success");
+            resetFields();
+        } else {
+            // error occurred when creating estimate
+            $("#submit-error-text").show();
+        }
+
     });
 
-    formFeedback.hide();
+    // enable submit button
+    $("#submit-button").removeAttr("disabled")
+        .removeClass("btn-success")
+        .addClass("button-default")
+        .text("Submit");
+    $("#submit-button-default-style").show();
+    $("#submitting-button-style").hide();
 });
 
 $("input").change(function (e) {
-    $(this).removeClass("is-invalid");
-    $("#"+ $(this).attr("id") + "-text").show();
+    // remove validation error attribute from current field if it has one
+    if ($(this).hasClass("is-invalid")) {
+        $(this).removeClass("is-invalid");
+
+        // restore default text
+        $("#"+ $(this).attr("id") + "-text").show();
+
+        // decrement total validation error count
+        totalErrors--;
+    }
+
+    // hide validation error text if all error fields have been resolved
+    if (totalErrors === 0) {
+        $("#submit-validation-error-text").hide();
+    }
 });
 
 
+function resetFields() {
+    // contact info
+    $("#name").val("");
+    $("#phone-number").val("");
+    $("#email").val("");
+    $("#email-contact").val("");
+    $("#phone-radio-button")[0].checked = true;
+    $("#email-radio-button")[0].checked = false;
+
+    // address fields
+    $("#street-address").val("");
+    $("#address-line-2").val("");
+    $("#city").val("");
+    $("#zip-code").val("");
+
+    // project type fields
+    $("#exterior-painting")[0].checked = false;
+    $("#interior-painting")[0].checked = false;
+    $("#cabinets")[0].checked = false;
+    $("#fence-gate")[0].checked = false;
+    $("#other")[0].checked = false;
+
+    // heard from fields
+    $("#repeat-client")[0].checked = false;
+    $("#referral")[0].checked = false;
+    $("#email-hear")[0].checked = false;
+    $("#internet-ad")[0].checked = false;
+    $("#van-wrap")[0].checked = false;
+    $("#none")[0].checked = false;
+
+    // project details field
+    $("#project-details").val("");
+}
